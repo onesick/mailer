a884061a9cd609f13416d449b0a2e7decd059c4d
Wonsik Jeong <wsjeong21@hotmail.com>
2016-02-08 12:32:26 -0500

    tested curl for token checks and works

--- app.js
+++ app.js
@@ -15,18 +15,18 @@ app.get("/new", function(req, res){
 app.post("/contact", contactController.create);
 
 
-// app.all('/*', function(req, res, next) {
-//  // CORS headers
-//  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-//  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//  // Set custom headers for CORS
-//  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-//  if (req.method == 'OPTIONS') {
-//    res.status(200).end();
-//  } else {
-//    next();
-//  }
-// });
+app.all('/*', function(req, res, next) {
+ // CORS headers
+ res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+ res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+ // Set custom headers for CORS
+ res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+ if (req.method == 'OPTIONS') {
+   res.status(200).end();
+ } else {
+   next();
+ }
+});
 
 app.all('/api/v1/*', [/*bodyParser,*/ require('./middleware/jwtauth')]);
 app.use('/', require('./router'));
